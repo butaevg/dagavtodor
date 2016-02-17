5586360191fd92e3eebbd606bf2843eb50583538
German <gjet@live.ru>
2016-02-17 15:26:17 +0300

    modified models upload_to folder

--- pressa/models.py
+++ pressa/models.py
@@ -18,7 +18,7 @@ class Advert(models.Model):
 class Article(models.Model):
     name = models.CharField(max_length=512, verbose_name='Заголовок')
     body = models.TextField(verbose_name='Текст')
-    pic = models.ImageField(max_length=250, blank=True, upload_to='upload/smi', verbose_name='Фото')
+    pic = models.ImageField(max_length=250, blank=True, upload_to='articles/%Y-%m-%d/', verbose_name='Фото')
     putdate = models.DateField(verbose_name='Дата')
     source = models.CharField(max_length=512, blank=True, verbose_name='Ссылка на источник')
     source_name = models.CharField(max_length=100, blank=True, verbose_name='Источник')
@@ -42,7 +42,7 @@ class PhotoCat(models.Model):
 
 
 class Photo(models.Model):
-    url = models.ImageField(max_length=250, upload_to='upload/photo', verbose_name='Фото')
+    url = models.ImageField(max_length=250, upload_to='photo', verbose_name='Фото')
     cat = models.ForeignKey(PhotoCat, related_name="cat", verbose_name='Альбом')
 
     def image_tag(self):
@@ -59,7 +59,7 @@ class Post(models.Model):
     name = models.CharField(max_length=512, verbose_name='Заголовок')
     source = models.CharField(max_length=100, blank=True, verbose_name='Источник')
     body = models.TextField(verbose_name='Текст')
-    mainpic = models.ImageField(max_length=250, upload_to='upload/news', verbose_name='Картинка на главной')
+    mainpic = models.ImageField(max_length=250, upload_to='news/%Y-%m-%d/', verbose_name='Картинка на главной')
     putdate = models.DateField(verbose_name='Дата')
     
     def get_absolute_url(self):
@@ -70,7 +70,7 @@ class Post(models.Model):
 
 
 class PostImg(models.Model):
-    pic = models.ImageField(max_length=250, upload_to='upload/news', verbose_name='Фотографии к новости')
+    pic = models.ImageField(max_length=250, upload_to='news/%Y-%m-%d/', verbose_name='Фотографии к новости')
     post = models.ForeignKey(Post, related_name="pics")
 
 
@@ -79,7 +79,7 @@ class Question(models.Model):
     city = models.CharField(blank=True, max_length=100, verbose_name='Город')
     email = models.EmailField(blank=True, verbose_name='Адрес электр. почты')
     msg = models.TextField(verbose_name='Текст обращения *')
-    img = models.ImageField(blank=True, upload_to='upload/faq', verbose_name='Прилагаемoе фото')
+    img = models.ImageField(blank=True, upload_to='faq/%Y-%m-%d/', verbose_name='Прилагаемoе фото')
     answer = models.TextField(blank=True, verbose_name='Ответ')
     putdate = models.DateField(auto_now_add=True) 
     hide = models.BooleanField(default=True)
