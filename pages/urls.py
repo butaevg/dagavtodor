6abc5e34e0c772989795fe1ba07a3bebc7fcdf57
German <gjet@live.ru>
2016-03-18 17:37:09 +0300

    removed all apps to apps folder

--- pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from .views import PageDetail
-
-urlpatterns = [
-    url(r'^$', views.mainpage),
-    url(r'^(?P<slug>[\w-]+)/$', PageDetail.as_view()), 
-] 
