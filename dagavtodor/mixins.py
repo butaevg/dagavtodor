d092aab167ae222bf1e29d931e2b9c5e65bfecf0
German <gjet@live.ru>
2016-06-22 10:55:24 +0300

    dagavtodor -> core

--- dagavtodor/mixins.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required 
-from django.utils.decorators import method_decorator
-from django.http import HttpResponse, HttpResponseRedirect
-
-class LoginRequiredMixin(object):
-    @method_decorator(login_required)
-    def dispatch(self, *args, **kwargs):
-        return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
