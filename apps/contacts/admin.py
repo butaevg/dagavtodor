28c6208a02edace55163cadeb4265f28a9416056
german <gjet@live.ru>
2016-10-13 14:45:48 +0300

    some changes

--- apps/contacts/admin.py
+++
@@ -1,9 +0,0 @@
-#coding: utf-8
-from django.contrib import admin
-from .models import Department, Employee
-
-class EmployeeAdmin(admin.ModelAdmin):
-	list_display = ('name', 'dep',)
-
-admin.site.register(Department)
-admin.site.register(Employee, EmployeeAdmin)
--- apps/contacts/apps.py
+++
@@ -1,6 +0,0 @@
-#coding: utf-8
-from django.apps import AppConfig
-
-class ContactsAppConfig(AppConfig):
-    name = "contacts" 
-    verbose_name = u"Контакты" 
